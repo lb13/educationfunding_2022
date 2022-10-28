0ff273ac4bb3e61bbb3932e0a5cbb2c3d42f4954
Luke Burns <lbburns13@gmail.com>
2022-10-28 12:56:26 +0100

    Update ilr_rule_lookup.js

--- educationfunding_2022/static/js/ilr_rule_lookup.js
+++ educationfunding_2022/static/js/ilr_rule_lookup.js
@@ -1,6 +1,6 @@
 const ruleInput = document.getElementById("rule-input");
 const ruleResults = document.getElementById("rule-results");
-const ruleRequest = new Request("/data/ilr_rules_2223_v3.json");
+const ruleRequest = new Request("/data/ilr_rules_2223_v4.json");
 
 fetch(ruleRequest)
     .then(response => response.json())
